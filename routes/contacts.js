9ae2cf6bcb6d6fb3bd87acd43043bb75f767247e
javiera-MLJ <lor24003@byui.edu>
2026-01-24 20:49:01 -0300

    contact 2 complete

--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,10 @@ router.get('/', contactController.getAll);
 
 router.get('/:id', contactController.getSingle);
 
+router.post('/', contactController.createContact);
+
+router.put('/:id', contactController.updateContact);
+
+router.delete('/:id', contactController.deleteContact);
+
 module.exports = router;
